a8a0175ef551fbac96225cb0614f2d977741ff41
iaSokolov <iasokolov@gmail.com>
2018-10-12 11:01:20 +0300

    Добавьте файлы проекта.

---
+++ Tutorial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            a a1 = null;
+            if(a1 != null)
+            {
+                Console.WriteLine(a1.value);
+            }
+
+        }
+    }
+
+    class a
+    {
+        public int value;
+        public a(int _value)
+        {
+            value = _value;
+        }
+    }
+}
